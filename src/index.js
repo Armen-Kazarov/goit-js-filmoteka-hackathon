059c5ec65d218f027146a8b340aa248ad1bc85d7
Armen-Kazarov <dunkan_mc_laud@ukr.net>
2020-10-30 00:23:41 +0200

    add styles for section form, include fontawesome styles, add font droid sans

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './sass/main.scss';
 import 'material-design-icons/iconfont/material-icons.css';
 import '@fortawesome/fontawesome-free';
+import '@fortawesome/fontawesome-free/js/fontawesome.min';
+import '@fortawesome/fontawesome-free/js/all.min';
 import './js/1initialHomePage';
 import './js/2searchAndPlaginationHomePage';
 import './js/3navigation';
 import './js/4filmDetailsPage';
 import './js/5libraryPage';
-
