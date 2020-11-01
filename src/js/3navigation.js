f2df996f07258b56bb7da2c3399fdbca4ad62a57
Nikolaienko Ihor <nikolasha8876@gmail.com>
2020-11-01 10:44:03 +0200

    fixed button library

--- src/js/3navigation.js
+++ src/js/3navigation.js
@@ -1,6 +1,9 @@
-import {fetchPopularMoviesList} from './1initialHomePage.js';
-import {showDetails, drawQueueFilmList} from './4filmDetailsPage';
-import createLibraryElements from './5libraryPage.js';
+import { fetchPopularMoviesList } from './1initialHomePage.js';
+import { showDetails } from './4filmDetailsPage';
+import {
+  drawQueueFilmList,
+  drawWatchedFilmList,
+} from './5libraryPage.js';
 
 let selectFilm;
 
@@ -8,46 +11,50 @@ const exChange = document.querySelector('.js-films-list');
 
 const mainRef = document.querySelector('.main');
 
+const formaRef = document.querySelector('.search-film');
+
+const libraryBtnRef = document.querySelector('.library__btn__wrapper');
+
 const home = document.querySelector('.home-button');
 home.addEventListener('click', activeHomePage);
 const hederName = document.querySelector('.logo');
 hederName.addEventListener('click', activeHomePage);
 
-function activeHomePage () {
+function activeHomePage() {
   exChange.innerHTML = '';
-  fetchPopularMoviesList()
+  libraryBtnRef.classList.add('js-display__none');
+  formaRef.classList.remove('js-display__none');
+  fetchPopularMoviesList();
   //.addEventListener('click', кнопки пагинации);
   //.addEventListener('click', кнопки пагинации);
-};
+}
 
 const library = document.querySelector('.library-button');
 library.addEventListener('click', activeLibraryPage);
 
-function activeLibraryPage () {
+function activeLibraryPage() {
   exChange.innerHTML = '';
-  createLibraryElements();
+  formaRef.classList.add('js-display__none');
+  libraryBtnRef.classList.remove('js-display__none');
   drawQueueFilmList();
   const buttonQueue = document.querySelector('.js-btnQueue');
   const buttonWatched = document.querySelector('.js-btnWatched');
   buttonQueue.setAttribute('active', true);
-  buttonQueue.addEventListener('click', )
-  buttonWatched.addEventListener('click', )
-};
+  buttonQueue.addEventListener('click');
+  buttonWatched.addEventListener('click');
+}
 
-function activeDetailsPage (movieId) {
-  exChange.classList.add("hideAllLi");
+function activeDetailsPage(movieId) {
+  exChange.classList.add('hideAllLi');
   showDetails(selectFilm);
   mainRef.insertAdjacentHTML('beforeend', showDetails);
   const detailsQueue = document.querySelector('.details__queue');
   const detailsWatched = document.querySelector('.details__watched');
-  detailsQueue.addEventListener('click', )
-  detailsWatched.addEventListener('click', )
-};
+  detailsQueue.addEventListener('click');
+  detailsWatched.addEventListener('click');
+}
 
-
-
-const selectFilms =
-(function () {
+const selectFilms = (function () {
   const trackScroll = () => {
     let scrolled = window.pageYOffset;
     let coords = document.documentElement.clientHeight;
@@ -71,3 +78,7 @@ const selectFilms =
   goTopBtn.addEventListener('click', backToTop);
   window.addEventListener('scroll', trackScroll);
 })();
+
+export {
+  activeDetailsPage
+}
