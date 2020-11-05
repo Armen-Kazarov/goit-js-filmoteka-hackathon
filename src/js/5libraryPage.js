71367b9ffe941b0cadaab2e9a43469b8d80d132f
Nikolaienko Ihor <nikolasha8876@gmail.com>
2020-11-06 01:55:46 +0200

    fix function

--- src/js/5libraryPage.js
+++ src/js/5libraryPage.js
@@ -1,26 +1,27 @@
 import itemsLibraryTemplate from '../templates/itemLibraryTemplate.hbs';
-// import { apiKey } from '../js/1initialHomePage';
+import { apiKey } from './1initialHomePage';
 const libraryListRef = document.querySelector('.js-films-list');
 const queueBtnRef = document.querySelector('.js-btnQueue');
 const watchedBtnRef = document.querySelector('.js-btnWatched');
 
 const filmsFromLibrary = movieId => {
-  const urlForLibraryFilm = `https://api.themoviedb.org/3/movie/${movieId}?api_key=fa9fa54083c479003851c965e04509d5`;
+  const createLibraryCardFunc = data => {
+    const acc = [data];
+    libraryListRef.insertAdjacentHTML('beforeend', itemsLibraryTemplate(acc));
+  };
+  const urlForLibraryFilm = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`;
   fetch(urlForLibraryFilm)
     .then(res => res.json())
-    .then(data => {createLibraryCardFunc(data)})
-   
-};
-const createLibraryCardFunc = data => {
-const arr = [data]
-  libraryListRef.insertAdjacentHTML("beforeend", itemsLibraryTemplate(arr));
+    .then(data => {
+      createLibraryCardFunc(data);
+    });
 };
 
 const drawQueueFilmList = () => {
   queueBtnRef.classList.add('btn__active');
   watchedBtnRef.classList.remove('btn__active');
   const localStorageData = JSON.parse(localStorage.getItem('filmsQueue'));
-  if (localStorageData === null || localStorageData.length === null) {
+  if (localStorageData === null || localStorageData.length === 0) {
     libraryListRef.innerHTML =
       '<li class="content__warning__message">You do not have to queue movies to watch. Add them.</li>';
   } else {
@@ -33,7 +34,7 @@ const drawWatchedFilmList = () => {
   watchedBtnRef.classList.add('btn__active');
   queueBtnRef.classList.remove('btn__active');
   const localStorageData = JSON.parse(localStorage.getItem('filmsWatched'));
-  if (localStorageData === null || localStorageData.length === null) {
+  if (localStorageData === null || localStorageData.length === 0) {
     libraryListRef.innerHTML =
       '<li class="content__warning__message">You do not have watched movies. Add them.</li>';
   } else {
@@ -42,4 +43,4 @@ const drawWatchedFilmList = () => {
   }
 };
 
-export { createLibraryCardFunc, drawQueueFilmList, drawWatchedFilmList };
+export { drawQueueFilmList, drawWatchedFilmList };
